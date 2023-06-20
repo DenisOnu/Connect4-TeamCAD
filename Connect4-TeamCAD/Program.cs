78de910ada1a06360a2ea07657b937ce128ba514
Denis Onu <denisonu7@gmail.com>
2023-06-20 00:58:18 -0600

    initial connect 4 skeleton commit

--- Connect4-TeamCAD/Program.cs
+++ Connect4-TeamCAD/Program.cs
@@ -1,11 +1,77 @@
 ﻿using System;
 
-class Program
+public abstract class Player
 {
-    static void Main()
+    //initialize discs
+
+    //get player moves
+}
+
+//define  HumanPlayer class that inherits from 'Player'
+public class HumanPlayer : Player
+{
+
+}
+
+public class Connect4Game
+{
+    public Connect4Game()
     {
-        string message = "Hello, World!";
-        Console.WriteLine(message);
-        Console.WriteLine(message);
+        InitializeBoard();
+    }
+
+    public void PlayGame()
+    {
+
+    }
+
+    //set all positions on the board to empty
+    private void InitializeBoard()
+    {
+
+    }
+
+    //displays current state of board
+    private void PrintBoard()
+    {
+
+    }
+
+    //drop disc in specified column and return resulting row
+    private int DropDisc(int column)
+    {
+
+    }
+
+    //check if player in the current index has won the game
+    private bool CheckWin(int row, int col)
+    {
+        //check horizontally
+
+        //check vertically
+
+        //check diagonally
+    }
+
+    //checks if board is full
+    private bool IsBoardFull()
+    {
+
+    }
+
+    //sets the player discs
+    private void SelectPlayers()
+    {
+
+    }
+}
+
+//instantiates the game
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Connect4Game game = new Connect4Game();
+        game.PlayGame();
     }
 }
