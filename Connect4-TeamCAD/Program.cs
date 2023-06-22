11f67dacb8b15e686ec739e2579cd05141ce73f3
DenisOnu <103238132+DenisOnu@users.noreply.github.com>
2023-06-22 06:39:02 -0600

    Error handling and program update

--- Connect4-TeamCAD/Program.cs
+++ Connect4-TeamCAD/Program.cs
@@ -2,15 +2,44 @@ using System;
 
 public abstract class Player
 {
-    //initialize discs
-    //big changes
-    //get player moves
+    //disc representing game pieces of 'X' or 'O'
+    protected internal char disc { get; }
+
+    //constructor sets value of 'disc'
+    public Player(char disc)
+    {
+        this.disc = disc;
+    }
+    //declare abstract method GetMove() to be implemented by derived classes
+    public abstract int GetMove();
 }
 
 //define HumanPlayer class that inherits from 'Player'
 public class HumanPlayer : Player
 {
+    //constructor calls the base class constructor and passes the 'disc' argument
+    public HumanPlayer(char disc) : base(disc)
+    {
+    }
 
+    //overrides the GetMove() method prompting player to enter number or quit
+    public override int GetMove()
+    {
+        Console.Write("Player {0}, enter column number (1-7) or 'q' to quit the game: ", disc);
+        string input = Console.ReadLine();
+
+        if (input.ToLower() == "q")
+            Environment.Exit(0);
+
+        if (int.TryParse(input, out int column))
+        {
+            if (column >= 1 && column <= 7)
+                return column - 1;
+        }
+
+        Console.WriteLine("Invalid move input. Please try again.");
+        return GetMove(); // Recursive call to get a valid move
+    }
 }
 
 public class Connect4Game
