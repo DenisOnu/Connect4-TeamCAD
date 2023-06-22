9b4cf86ec28ce573cbdd278ba2cd9d7867b3491b
ArmanGolkar <113958147+ArmanGolkar@users.noreply.github.com>
2023-06-21 23:21:15 -0600

    define game logic using class 'Connect4Game'

--- Connect4-TeamCAD/Program.cs
+++ Connect4-TeamCAD/Program.cs
@@ -1,52 +1,32 @@
-﻿using System;
+using System;
 
 public abstract class Player
 {
     //initialize discs
-    //disc representing game pieces of 'X' or 'O'
-    protected internal char disc { get; }
-    //constructor sets value of 'disc'
-    public Player(char disc)
-    {
-        this.disc = disc;
-    }
+    //big changes
     //get player moves
-    //declare abstract method GetMove() to be implemented by derived classes
-    public abstract int GetMove();
 }
 
-//define  HumanPlayer class that inherits from 'Player'
+//define HumanPlayer class that inherits from 'Player'
 public class HumanPlayer : Player
 {
-    //constructor calls the base class constructor and passes the 'disc' argument
-    public HumanPlayer(char disc) : base(disc)
-    {
-    }
 
-    //overrides the GetMove() method prompting player to enter number or quit
-    public override int GetMove()
-    {
-        Console.Write("Player {0}, enter column number (1-7) or 'q' to quit the game: ", disc);
-        string input = Console.ReadLine();
-
-        if (input.ToLower() == "q")
-            Environment.Exit(0);
-
-        if (int.TryParse(input, out int column))
-        {
-            if (column >= 1 && column <= 7)
-                return column - 1;
-        }
-
-        Console.WriteLine("Invalid move input. Please try again.");
-        return GetMove(); // Recursive call to get a valid move
-    }
 }
 
 public class Connect4Game
 {
+    private char[,] board;
+    private Player[] players;
+    private int currentPlayerIndex;
+    private bool gameOver;
+
+    //constructor initializes the members
     public Connect4Game()
     {
+        board = new char[6, 7];
+        players = new Player[2];
+        currentPlayerIndex = 0;
+        gameOver = false;
         InitializeBoard();
     }
 
@@ -61,19 +41,19 @@ public class Connect4Game
 
     }
 
-    //displays current state of board
+    //displays the current state of the board
     private void PrintBoard()
     {
 
     }
 
-    //drop disc in specified column and return resulting row
+    //drop a disc in the specified column and return the resulting row
     private int DropDisc(int column)
     {
 
     }
 
-    //check if player in the current index has won the game
+    //check if the player in the current index has won the game
     private bool CheckWin(int row, int col)
     {
         //check horizontally
@@ -83,7 +63,7 @@ public class Connect4Game
         //check diagonally
     }
 
-    //checks if board is full
+    //check if the board is full
     private bool IsBoardFull()
     {
 
